12eda59b5b8cd000a81312bd7fb1ed3b03dcb4c4
Hamilton Valério Filho <htecmac@gmail.com>
2022-04-16 15:58:23 -0300

    First Commit
    First Commit

---
+++ cadastro/src/test/java/br/eti/valerio/cestoque/cadastro/HelloResourceIT.java
@@ -0,0 +1,9 @@
+package br.eti.valerio.cestoque.cadastro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
